fbad616ecf7368d4b3336a713d877cc943382b6b
emonteroe <emanuel.montero.e@gmail.com>
2020-01-22 17:19:35 -0300

    script to clean traces

---
+++ cleantraces.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  cleantraces.sh
+# 
+#         USAGE:  ./cleantraces.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  01/22/20 17:17:49 -03
+#      REVISION:  ---
+#===============================================================================
+
+rm Dl*
+rm Ul*
