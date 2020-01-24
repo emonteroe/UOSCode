19eca510d951fb7670ca70c4d7ab7cd21056f707
emonteroe <emanuel.montero.e@gmail.com>
2020-01-24 14:48:02 -0300

    script to move result files to result folder

---
+++ move_result_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  move_result_files.sh
+# 
+#         USAGE:  ./move_result_files.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  01/24/20 13:19:05 -03
+#      REVISION:  ---
+#===============================================================================
+
+mv APD* results_thesis
+mv Jitter* results_thesis
+mv PLR* results_thesis
+mv PDR* results_thesis
+mv Throughput* results_thesis
+mv UE_info* results_thesis
+mv Quantity* results_thesis    
+mv UOSLTE* results_thesis
+mv UABSs results_thesis
+mv UEsL* results_thesis
+mv Dl* results_thesis
+mv Ul* results_thesis   
